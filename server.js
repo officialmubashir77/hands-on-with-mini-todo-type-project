e62ae517371573355c4487c78245836294f23ee8
officialmubashir77 <officialmubashir77@gmail.com>
2024-08-07 16:56:55 -0700

    hands-on-with-mini-todo-type-project

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const fs = require('fs');
+const port = 3000;
+
+// middleware for parsing JSON requests
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({extended : true}));
+app.use(express.static(path.join(__dirname , "public")));
+
+app.get('/', (req, res) => {
+  fs.readdir('./files' , (err, files) => {
+    res.render('index' , {files : files});
+  })
+});
+
+//post form data route 
+app.post('/create', (req, res) => {
+    // console.log(req.body);
+    fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt` , req.body.details , (err) => {
+        if(err) throw err;
+        console.log('File created');
+        res.redirect('/');
+    })
+});
+
+// listen server
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
